6b32d4ce84697d8777efcb7bda8e15850126a93a
Yusuf Said Aktan <contact@ysaktan.com>
2025-02-14 15:49:44 +0300

    feat: spring boot init

---
+++ src/main/java/com/traveller/kivi/KiviApplication.java
@@ -0,0 +1,13 @@
+package com.traveller.kivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traveller/kivi/KiviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveller.kivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
