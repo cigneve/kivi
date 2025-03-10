037e3eba9a8d385042c75a0c1b67816f313a4398
Yusuf Said Aktan <contact@ysaktan.com>
2025-03-10 16:50:32 +0300

    feat(userController): init

---
+++ src/main/java/com/traveller/kivi/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.traveller.kivi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.traveller.kivi.model.users.User;
+import com.traveller.kivi.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/type/{userType}")
+    public List<User> getUsersByType(@PathVariable User.UserType userType) {
+        return userService.getUsersByUserType(userType, PageRequest.of(1, 10));
+    }
+
+}
