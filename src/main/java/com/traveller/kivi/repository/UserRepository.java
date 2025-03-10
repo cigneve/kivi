e0351cf1476f618047e9e8f66a855c1b4e6ab14b
Yusuf Said Aktan <contact@ysaktan.com>
2025-03-10 16:50:41 +0300

    feat: user repository

---
+++ src/main/java/com/traveller/kivi/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.traveller.kivi.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.traveller.kivi.model.users.User;
+import com.traveller.kivi.model.users.User.UserType;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findByUserType(User.UserType userType);
+
+    List<User> getUsersByUserType(UserType userType, Pageable pageable);
+}
