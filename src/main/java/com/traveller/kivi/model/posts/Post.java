6e1b4ea30bd592c8c65833989a5d7bcf4074af23
Yusuf Said Aktan <contact@ysaktan.com>
2025-04-22 11:15:44 +0300

    feat(Post): add post model

---
+++ src/main/java/com/traveller/kivi/model/posts/Post.java
@@ -0,0 +1,22 @@
+package com.traveller.kivi.model.posts;
+
+import java.util.List;
+import java.util.Set;
+
+import com.traveller.kivi.model.users.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Post {
+    @Id
+    private Integer id;
+
+    @OneToMany
+    private Set<User> likers;
+
+    @OneToMany(mappedBy = "post")
+    private List<PostComment> comments;
+}
