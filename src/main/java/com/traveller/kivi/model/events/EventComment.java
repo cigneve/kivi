8ed34f8ec47296103bdb508c0f3803375278c99f
Yusuf Said Aktan <contact@ysaktan.com>
2025-04-22 10:07:17 +0300

    feat(Event): add event comments

--- src/main/java/com/traveller/kivi/model/events/EventComment.java
+++ src/main/java/com/traveller/kivi/model/events/EventComment.java
@@ -1,5 +1,32 @@
 package com.traveller.kivi.model.events;
 
-public class EventComment {
+import java.time.LocalDate;
 
+import com.traveller.kivi.model.users.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class EventComment {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    private Event event;
+
+    @NotNull
+    private User owner;
+
+    @NotNull
+    private LocalDate commentDate;
+
+    @NotNull
+    @NotEmpty
+    private String commentBody;
 }
