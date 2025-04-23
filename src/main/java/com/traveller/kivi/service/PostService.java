db4d9414a7f661981315524dcc5275ea6a6e25b9
Yusuf Said Aktan <contact@ysaktan.com>
2025-04-23 20:32:15 +0300

    feat(Post): add PostRepository

--- src/main/java/com/traveller/kivi/service/PostService.java
+++ src/main/java/com/traveller/kivi/service/PostService.java
@@ -1,8 +1,13 @@
 package com.traveller.kivi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.traveller.kivi.repository.PostRepository;
 
 @Service
 public class PostService {
+    @Autowired
+    private PostRepository postRepository;
 
 }
