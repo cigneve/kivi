2d9ae723f061d064e2f3be5b9e609c67d19f3a29
Yusuf Said Aktan <contact@ysaktan.com>
2025-04-23 22:33:39 +0300

    feat(Post): add basic service methods
    createPost getPost getPaginatedPosts

--- src/main/java/com/traveller/kivi/service/PostService.java
+++ src/main/java/com/traveller/kivi/service/PostService.java
@@ -1,8 +1,11 @@
 package com.traveller.kivi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.traveller.kivi.model.posts.Post;
 import com.traveller.kivi.repository.PostRepository;
 
 @Service
@@ -10,4 +13,15 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    public Post getPost(Integer postId) {
+        return postRepository.findById(postId).get();
+    }
+
+    public Page<Post> getPaginatedPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
 }
