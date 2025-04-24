210ad90ce9d41a37a774bd6243863ca5417342c1
Yusuf Said Aktan <contact@ysaktan.com>
2025-04-24 18:10:50 +0300

    feat(Post): add owner (User)

--- src/main/java/com/traveller/kivi/model/posts/Post.java
+++ src/main/java/com/traveller/kivi/model/posts/Post.java
@@ -8,7 +8,9 @@ import com.traveller.kivi.model.users.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
 
 /**
  * Post is a post by an user that is presented in the social media feed.
@@ -25,6 +27,10 @@ public class Post {
     private Set<PostTag> tags;
 
     private String body;
+
+    @ManyToOne
+    @NotNull
+    private User owner;
 
     @OneToMany(mappedBy = "post")
     private List<PostComment> comments;
