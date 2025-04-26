15bd3ad8641412057f14de99f5ea81c1d45f083c
Yusuf Said Aktan <contact@ysaktan.com>
2025-04-26 13:17:41 +0300

    feat(Post): add Image uploading

--- src/main/java/com/traveller/kivi/service/PostService.java
+++ src/main/java/com/traveller/kivi/service/PostService.java
@@ -1,11 +1,15 @@
 package com.traveller.kivi.service;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.traveller.exception.UserNotFoundException;
+import com.traveller.kivi.model.Image;
 import com.traveller.kivi.model.posts.Post;
 import com.traveller.kivi.model.posts.PostDTO;
 import com.traveller.kivi.model.users.User;
@@ -15,6 +19,9 @@ import com.traveller.kivi.repository.PostRepository;
 public class PostService {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private ImageService imageService;
 
     @Autowired
     private PostRepository postRepository;
@@ -42,7 +49,12 @@ public class PostService {
             throw new UserNotFoundException("User with ID " + postDTO.getUserId() + " not found");
         }
         User owner = userService.getUserById(postDTO.getUserId());
-        Post constructPost = new Post(owner, postDTO.getBody(), postDTO.getImages());
+        Post constructPost = new Post(owner, postDTO.getBody(), new ArrayList<>());
+        for (String image : postDTO.getImages()) {
+            String imageId = new StringBuilder().append(owner.getId()).append(UUID.randomUUID().toString()).toString();
+            Image imageObj = imageService.createImage(imageId, image);
+            constructPost.getImages().add(imageObj);
+        }
         constructPost.setBody(postDTO.getBody());
         return postRepository.save(constructPost);
     }
