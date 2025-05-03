e27923329db3f4057a1eb4aa8e02f8f39e478811
Kerem <kcinar2005@gmail.com>
2025-05-03 13:48:22 +0300

    created service and controller classes for achievements

---
+++ src/main/java/com/traveller/kivi/controller/AchievementController.java
@@ -0,0 +1,24 @@
+package com.traveller.kivi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import com.traveller.kivi.model.achievements.UserAchievement;
+import com.traveller.kivi.service.AchievementService;
+
+
+@RestController
+@RequestMapping("/api/achievements")
+public class AchievementController {
+
+    @Autowired
+    private AchievementService achievementService;
+
+    /**
+     * Kullanıcının kazandığı rozetleri döner.
+     */
+    @GetMapping("/users/{userId}")
+    public List<UserAchievement> getUserAchievements(@PathVariable Integer userId) {
+        return achievementService.getUserAchievements(userId);
+    }
+}
