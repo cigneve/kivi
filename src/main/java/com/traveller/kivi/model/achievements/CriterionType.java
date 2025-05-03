65fd7e44435b3e9bbf5165c4745948b87f20cc6e
Kerem <kcinar2005@gmail.com>
2025-05-03 13:49:15 +0300

    CriterionType class

---
+++ src/main/java/com/traveller/kivi/model/achievements/CriterionType.java
@@ -0,0 +1,11 @@
+package com.traveller.kivi.model.achievements;
+
+public enum CriterionType {
+    EVENT_CREATE,
+    EVENT_JOIN,
+    POST_CREATE,
+    IMAGE_UPLOAD,
+    COMMENT_WRITE,
+    LIKE_RECEIVE
+}
+
