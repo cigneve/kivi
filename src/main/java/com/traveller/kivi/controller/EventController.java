217ca35ade9367a80a853db485d3537cafc99bf4
Yusuf Said Aktan <contact@ysaktan.com>
2025-05-06 16:05:32 +0300

    feat(Event): use Event DTO as api format

--- src/main/java/com/traveller/kivi/controller/EventController.java
+++ src/main/java/com/traveller/kivi/controller/EventController.java
@@ -7,9 +7,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.traveller.kivi.model.events.Event;
+import com.traveller.kivi.model.events.dto.EventDetails;
 import com.traveller.kivi.service.EventService;
 
 import jakarta.validation.Valid;
@@ -22,26 +30,25 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
-    @Autowired
     public EventController(EventService eventService) {
-        this.eventService= eventService;
+        this.eventService = eventService;
     }
 
     /**
-     * Creates new event. 
+     * Creates new event.
      */
     @PostMapping
-    public ResponseEntity<Event> createEvent(@Valid @RequestBody Event event) {
+    public ResponseEntity<EventDetails> createEvent(@Valid @RequestBody Event event) {
 
-        Event created = eventService.createEvent(event);
+        EventDetails created = eventService.createEvent(event);
         return ResponseEntity.ok(created);
     }
 
     /**
-     * Returns the list of all events. 
+     * Returns the list of all events.
      */
     @GetMapping("/all")
-    public List<Event> getAllEvents() {
+    public List<EventDetails> getAllEvents() {
         return eventService.getAllEvents();
     }
 
@@ -49,7 +56,7 @@ public class EventController {
      * Returns paginated list of events.
      */
     @GetMapping
-    public PagedModel<Event> getPaginatedEvents(Pageable pageable) {
+    public PagedModel<EventDetails> getPaginatedEvents(Pageable pageable) {
         return new PagedModel<>(eventService.getPaginatedEvents(pageable));
     }
 
@@ -58,7 +65,7 @@ public class EventController {
      */
     @GetMapping("/{eventId}")
     public ResponseEntity<Event> getEvent(@PathVariable Integer eventId) {
-        if(!eventService.eventExistsById(eventId)) {
+        if (!eventService.eventExistsById(eventId)) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(eventService.getEventById(eventId));
@@ -68,11 +75,11 @@ public class EventController {
      * Updates an event.
      */
     @PutMapping("/{eventId}")
-    public ResponseEntity<Event> updateEvent(@PathVariable Integer eventId, @Valid @RequestBody Event updated) {
-        if(!eventService.eventExistsById(eventId)) {
+    public ResponseEntity<EventDetails> updateEvent(@PathVariable Integer eventId, @Valid @RequestBody Event updated) {
+        if (!eventService.eventExistsById(eventId)) {
             return ResponseEntity.notFound().build();
         }
-        Event result = eventService.updateEvent(eventId, updated);
+        EventDetails result = eventService.updateEvent(eventId, updated);
         return ResponseEntity.ok(result);
     }
 
@@ -81,7 +88,7 @@ public class EventController {
      */
     @DeleteMapping("/{eventId}")
     public ResponseEntity<Void> deleteEvent(@PathVariable Integer eventId) {
-        if(!eventService.eventExistsById(eventId)) {
+        if (!eventService.eventExistsById(eventId)) {
             return ResponseEntity.notFound().build();
         }
         eventService.deleteEvent(eventId);
