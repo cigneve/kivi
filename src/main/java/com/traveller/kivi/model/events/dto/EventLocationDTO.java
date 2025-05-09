aff3f46c9d45c35d168f4e67b3c08bd6bd41807a
Yusuf Said Aktan <contact@ysaktan.com>
2025-05-09 12:37:53 +0300

    feat(EventLocation): add DTO

---
+++ src/main/java/com/traveller/kivi/model/events/dto/EventLocationDTO.java
@@ -0,0 +1,44 @@
+package com.traveller.kivi.model.events.dto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.traveller.kivi.model.events.Coordinate;
+import com.traveller.kivi.model.events.EventLocation;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table
+public class EventLocationDTO {
+    public static EventLocationDTO fromEventLocation(EventLocation eventLocation) {
+        var dto = new EventLocationDTO();
+        dto.id = eventLocation.getId();
+        dto.location = eventLocation.getLocation();
+        dto.title = eventLocation.getTitle();
+        dto.description = eventLocation.getDescription();
+        dto.keywords = eventLocation.getKeywords();
+        dto.featured = eventLocation.isFeatured();
+        return dto;
+    }
+
+    @NotNull
+    public Integer id;
+
+    @NotNull
+    public Coordinate location;
+
+    @NotBlank
+    public String title;
+
+    @NotBlank
+    public String description;
+
+    public Set<String> keywords = new HashSet<>();
+
+    public boolean featured;
+
+}
