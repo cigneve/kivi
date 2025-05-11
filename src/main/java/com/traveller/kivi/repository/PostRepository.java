e2be869340d5fa3a7a06fb7a3a788865b1538293
Kerem <kcinar2005@gmail.com>
2025-05-11 16:43:15 +0300

    addition to PostRepository

--- src/main/java/com/traveller/kivi/repository/PostRepository.java
+++ src/main/java/com/traveller/kivi/repository/PostRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.traveller.kivi.model.posts.Post;
@@ -18,4 +20,10 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByOwner(User user);
 
     Long countByOwner_Id(Integer ownerId);
+
+    /**
+     * Total number of likes (across all posts)
+     */
+    @Query("SELECT COUNT(l) FROM Post p JOIN p.likers l WHERE p.owner.id = :ownerId")
+    Long countLikesByOwner_Id(@Param("ownerId") Integer ownerId);
 }
